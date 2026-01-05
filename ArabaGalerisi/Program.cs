d01451bb43584d1d37fe38b77baf62b9058b7249
kacak <kacak@MASTER>
2026-01-05 18:27:58 +0300

    Add project files.

---
+++ ArabaGalerisi/Program.cs
@@ -0,0 +1,11 @@
+﻿// 1 -) Yeni araç
+// 2-) Araçları listele
+// 3-) Araç sil
+// 4-) Araç arama
+// 5-) Toplam araç sayısı
+// 6-) İndirim Yap (Maks. %20)
+// 7-) Zam Yap (Maks. %20) 0-)
+// 0-) Çıkış
+// -- Yukarıdaki menü yapısı üzerinden bir araba galerisi uygulaması yapılacak.
+// -- Arabaların (Marka, Model, Model Yılı, Renk, Fiyat) özellikleri olacak.
+// -- 6 ve 7. maddelerdeki zam ve indirim oranları tüm araçları etkileyecek ve miktar girildiğinde yüzdelik olarak zam veya indirim uygulanacak
